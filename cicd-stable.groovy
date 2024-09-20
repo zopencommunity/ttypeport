cec330c99e5016d01949237d24c98a67170e676e
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:43 -0400

    Change organization to zopencommunity

--- cicd-stable.groovy
+++ cicd-stable.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/ttypeport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/ttypeport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/ttypeport.git'), string(name: 'PORT_DESCRIPTION', value: 'A Terminal-Based Typing App' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/ttypeport.git'), string(name: 'PORT_DESCRIPTION', value: 'A Terminal-Based Typing App' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
   }
 }
